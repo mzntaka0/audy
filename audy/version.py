f3f990d3af53e8be78124eebc44aaeec4523b2e8
mzntaka0 <takao.mizuno110@gmail.com>
2018-12-05 02:36:45 +0900

    updated version.py

---
+++ audy/version.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Metadata of package."""
+__version__ = '0.0.1'
+__title__ = 'audy'
+__description__ = 'Preprocessor for ASR, Speech Separation and Speech Enhancement.'
+__url__ = ''
+__author__ = 'Takao Mizuno'
+__author_email__ = ''
+__license__ = 'Apache Software License'
